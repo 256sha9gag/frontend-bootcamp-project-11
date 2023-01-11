a99e9c7946bd733431f1c5c6ae03f224dd4b5daa
gag <256sha9gag@gmail.com>
2023-01-11 15:02:01 +0300

    build: add deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      clean: true,
+    }),
+  ],
 };
