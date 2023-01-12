74de80db3c5507688ed26cdcac3231abcf04e6ba
gag <256sha9gag@gmail.com>
2023-01-12 13:25:33 +0300

    feat: add form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import _ from 'lodash';
+import './scss/styles.scss';
 
-function component() {
-  const element = document.createElement('div');
+const app = () => {
+  const container = document.createElement('div');
+  const form = document.createElement('form');
+  const inputDiv = document.createElement('div');
+  inputDiv.classList.add('mb-3');
+  const label = document.createElement('label');
+  label.setAttribute('for', 'url-input');
+  label.textContent = 'Ссылка RSS';
+  const input = document.createElement('input');
+  input.id = 'url-input';
+  form.appendChild(label);
+  form.appendChild(input);
+  const button = document.createElement('button');
+  button.setAttribute('type', 'submit');
+  button.classList.add('btn', 'btn-primary');
+  button.textContent = 'Добавить';
+  container.classList.add('container-fluid', 'd-flex', 'p-5', 'bg-dark');
+  container.appendChild(form);
+  container.appendChild(button);
+  document.body.appendChild(container);
+};
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+app();
