c2e192eb3212eb52abc6d13aeb1d52c984acbe44
gag <256sha9gag@gmail.com>
2023-01-16 15:04:40 +0300

    feat: add controller and model

--- src/init.js
+++ src/init.js
@@ -1,3 +1,15 @@
-export default () => {
+import validation from './validation.js';
 
+const controller = () => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    validation({ website: url });
+  });
+};
+
+export default () => {
+  controller();
 };
---
+++ src/model.js
@@ -0,0 +1,6 @@
+export default {
+  feeds: [],
+  status: 'RSS успешно загружен',
+  error: '',
+  isValid: null,
+};
