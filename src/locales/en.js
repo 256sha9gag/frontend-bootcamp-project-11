dffd6a56cfabde936a618b76c15820e4ad872046
gag <256sha9gag@gmail.com>
2023-01-17 09:48:09 +0300

    feat: add dictionary

---
+++ src/locales/en.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    title: 'RSS aggregator',
+    description: 'Start reading RSS today! It\'s easy, it\'s beautiful.',
+    examples: 'Examples: \nhttps://ru.hexlet.io/lessons.rss\nhttps://habr.com/ru/rss/hubs/all/\nhttp://static.feed.rbc.ru/rbc/logical/footer/news.rss',
+    inputContent: 'Link RSS',
+    button: {
+      add: 'Add',
+      view: 'view',
+      read: 'Read all',
+      close: 'Close',
+    },
+    posts: 'Posts',
+    feeds: 'Feeds',
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    title: 'RSS агрегатор',
+    description: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    examples: 'Примеры: \nhttps://ru.hexlet.io/lessons.rss\nhttps://habr.com/ru/rss/hubs/all/\nhttp://static.feed.rbc.ru/rbc/logical/footer/news.rss',
+    inputContent: 'Ссылка RSS',
+    button: {
+      add: 'Добавить',
+      view: 'Просмотр',
+      read: 'Читать полностью',
+      close: 'Закрыть',
+    },
+    posts: 'Посты',
+    feeds: 'Фиды',
+  },
+};
