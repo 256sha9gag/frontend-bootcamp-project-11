14fd320e505f45a82d1d645d37d15785e6858a3e
gag <256sha9gag@gmail.com>
2023-01-20 16:54:12 +0300

    refactor init.js

--- src/init.js
+++ src/init.js
@@ -1,32 +1,14 @@
 import i18n from 'i18next';
-import validation from './validation.js';
+import controllers from './controllers.js';
 import state from './model.js';
 import watchedState from './view.js';
 import resources from './locales/index.js';
-
-const controllers = () => {
-  const form = document.querySelector('form');
-  const changeLanguages = document.querySelectorAll('[data-lang="lang"]');
-
-  changeLanguages.forEach((button) => {
-    button.addEventListener('click', (e) => {
-      watchedState.lng = e.target.id;
-    });
-  });
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    form.focus();
-    const formData = new FormData(e.target);
-    const url = formData.get('url');
-    validation({ website: url });
-  });
-};
 
 const app = (i18next) => {
   state.i18nInstance = i18next;
   const defaultLanguage = 'ru';
   watchedState.lng = defaultLanguage;
+  state.state = 'filling';
   controllers();
 };
 
@@ -39,6 +21,6 @@ export default () => {
       resources,
     })
     .then((t) => { t('key'); })
-    .catch((e) => { throw new Error('ошибка', e); });
+    .catch((e) => { throw new Error('Error init i18nInstance', e); });
   return app(i18nInstance);
 };
