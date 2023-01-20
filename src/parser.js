7c17136fbb9e65062eb57ba0ed7e4233959fe3e4
gag <256sha9gag@gmail.com>
2023-01-20 16:59:52 +0300

    feat: add parser rss

---
+++ src/parser.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+export default (data, url) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data, 'application/xml');
+  const error = doc.querySelector('parsererror');
+  if (error) {
+    return null;
+  }
+  const feedTitle = doc.querySelector('channel > title').textContent;
+  const feedDescription = doc.querySelector('channel > description').textContent;
+  const idFeed = _.uniqueId();
+  const items = doc.querySelectorAll('item');
+  const posts = [];
+  items.forEach((item) => {
+    const itemTitle = item.querySelector('title').textContent;
+    const itemDescription = item.querySelector('description').textContent;
+    const itemLink = item.querySelector('link').textContent;
+    const idItem = _.uniqueId();
+    posts.push({
+      title: itemTitle,
+      description: itemDescription,
+      link: itemLink,
+      idItem,
+    });
+  });
+  const feed = {
+    title: feedTitle,
+    description: feedDescription,
+    link: url,
+    idFeed,
+    posts,
+  };
+
+  return feed;
+};
