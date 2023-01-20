ad2ca6418add1d047780127bdc2679e06b87609c
gag <256sha9gag@gmail.com>
2023-01-20 16:55:43 +0300

    refactor validation

--- src/validation.js
+++ src/validation.js
@@ -1,25 +1,16 @@
 import * as yup from 'yup';
-import watchedState from './view.js';
 import state from './model.js';
 
-const schema = yup.object().shape({
-  website: yup.string().required().url(),
-});
+const schema = yup.string().required().url();
 
-const validation = (web) => {
-  if (state.feeds.includes(web.website)) {
-    state.status = 'exist';
-    watchedState.isValid = false;
-  } else {
-    schema.validate(web)
-      .then((url) => state.feeds.push(url.website))
-      .then(() => { state.status = 'success'; })
-      .then(() => { watchedState.isValid = true; })
-      .catch(() => {
-        state.status = 'invalid';
-        watchedState.isValid = false;
-      });
+const validation = (url) => {
+  if (state.links.includes(url)) {
+    state.errors = 'exist';
   }
+  return schema.validate(url)
+    .catch(() => {
+      state.errors = 'invalidURL';
+    });
 };
 
 export default validation;
