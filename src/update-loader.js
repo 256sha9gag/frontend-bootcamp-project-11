fbf37e6ac4ab1b75f853f3ea7413f4f8dc586e54
gag <256sha9gag@gmail.com>
2023-01-30 11:57:31 +0300

    fix: update-loader

--- src/update-loader.js
+++ src/update-loader.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import watchedState from './view.js';
 import state from './model.js';
 import load from './loader.js';
 import parse from './parser.js';
@@ -6,17 +7,21 @@ import parse from './parser.js';
 const period = 5000;
 
 const checkUpdates = () => {
-  console.log('wow');
-  state.feeds.forEach((feed) => {
-    load(feed.link)
-      .then((data) => parse(data))
-      .then((parsed) => {
-        const diff = _.differenceBy(feed.posts, parsed.posts, 'title');
-        if (!_.isEmpty(diff)) {
-          console.log(diff);
-        }
+  if (!_.isEmpty(state.links)) {
+    const arr = state.links.map((link) => load(link)
+      .then((data) => {
+        const [, posts] = parse(data);
+        return posts;
+      }));
+    Promise.all(arr)
+      .then((a) => a.flat())
+      .then((newPosts) => {
+        watchedState.posts = [
+          ...(_.differenceWith(newPosts, state.posts, (obj, obj2) => obj.title === obj2.title)),
+          ...state.posts,
+        ];
       });
-  });
+  }
 };
 
 const loadUpdate = () => {
