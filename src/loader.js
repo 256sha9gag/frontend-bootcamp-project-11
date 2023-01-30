e89a34c4920755bf7f04d234c730a775e136db5b
gag <256sha9gag@gmail.com>
2023-01-30 11:54:13 +0300

    refactor: loader

--- src/loader.js
+++ src/loader.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
+import watchedState from './view.js';
+import state from './model.js';
 
 const getProxy = (url) => {
   const proxyURL = 'https://allorigins.hexlet.app';
   const proxy = new URL('get', proxyURL);
-  proxy.searchParams.append('disableCach', true);
+  proxy.searchParams.append('disableCache', true);
   proxy.searchParams.append('url', url);
   return proxy.href;
 };
 
 export default (url) => axios.get(getProxy(url))
-  .then((response) => response.data.contents);
+  .then((response) => response.data.contents)
+  .catch(() => {
+    state.errors = 'errorNetwork';
+    watchedState.state = 'failed';
+  });
